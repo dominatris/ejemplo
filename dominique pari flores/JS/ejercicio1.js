542a99aafb3acd1b7f76819d31d3454dafcd8c04
dominatris <132932919+dominatris@users.noreply.github.com>
2023-06-05 14:39:02 -0300

    ejer 1

---
+++ dominique pari flores/JS/ejercicio1.js
@@ -0,0 +1,7 @@
+function Ejercicio1() {
+    var meses = ["Enero","Febrero","Marzo","Abrir","Mayo","junio","julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+
+    for (let i = 0; i < meses.length; i++) {
+        console.log(meses[i]);
+    }
+}
--- dominique pari flores/JS/index.js
+++
@@ -1,7 +0,0 @@
-function Ejercicio1() {
-    var meses = ["Enero","Febrero","Marzo","Abrir","Mayo","junio","julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
-
-    for (let i = 0; i < meses.length; i++) {
-        console.log(meses[i]);
-    }
-}
