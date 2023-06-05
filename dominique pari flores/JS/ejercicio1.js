935d8b14347d4c2f1f6b72bce458236bcd744396
dominatris <132932919+dominatris@users.noreply.github.com>
2023-06-05 15:02:50 -0300

    ejer 1-4

--- dominique pari flores/JS/ejercicio1.js
+++ dominique pari flores/JS/ejercicio1.js
@@ -5,3 +5,87 @@ function Ejercicio1() {
         console.log(meses[i]);
     }
 }
+
+//ejercicico 22222222222222
+
+function Ejercicio2() {
+    var valores = [true, 5, false, "hola", "adios",2,"gjdjdflk"];
+    var valor = "";
+
+    for (let i = 0; i < valores.length; i++) {
+        if (typeof(valores[i]) == "string") {
+            if (valores[i].length > valor.length) {
+            valor = valores[i];
+        }
+    }
+}
+console.log("el mayor es "+valor);
+
+//comparacion directa
+if (valores[3].length > valores[4].length) {
+console.log("hola en mayor");
+}else{
+console.log("adios en mayor")
+}
+}
+
+function Ejercicio3() {
+
+    var numero1 = 5;
+    var numero2 = 8;
+if(numero1 < numero2) {
+console.log("numero1 no es mayor que numero2");
+}
+if(numero2 > 0) {
+console.log("numero2 es positivo");
+}
+if(numero1 < 0 || numero1 != 0) {
+console.log("numero1 es negativo o distinto de cero");
+}
+numero1 = numero1 + 1;
+if(numero2 > numero1) {
+console.log("Incrementar en 1 unidad el valor de numero1 no lo hace mayor o igual que numero2");
+}
+}
+
+
+
+function Ejercicio4(params) {
+    var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',
+'E', 'T'];
+
+var dni = document.querySelector("#entrada").value;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+function solicitudAJAX() {
+
+    var id = document.querySelector("#entrada").value;
+    var url = "https://pokeapi.co/api/v2/pokemon/"+id;
+
+    var objXMLHttpRequest = new XMLHttpRequest();
+
+    objXMLHttpRequest.onreadystatechange = function() {
+    if(objXMLHttpRequest.readyState === 4) {
+        if(objXMLHttpRequest.status === 200) {
+          console.log(objXMLHttpRequest.responseText);
+        }else {
+          alert('Error Code: ' +  objXMLHttpRequest.status);
+          alert('Error Message: ' + objXMLHttpRequest.statusText);
+        }
+    }
+}
+objXMLHttpRequest.open('GET', url);
+objXMLHttpRequest.send();
+}
